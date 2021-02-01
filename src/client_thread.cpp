0d67e9124e6f1dc64558fdc706688b188bf00ebc
Mark Harmstone <mark@harmstone.com>
2021-02-01 19:47:25 +0000

    client_thread: receive packets

--- src/client_thread.cpp
+++ src/client_thread.cpp
@@ -1,7 +1,65 @@
 #include "mariatds.h"
+#include <sys/socket.h>
 
 using namespace std;
 
+string client_thread::recv(unsigned int len) {
+    string s;
+    int bytes, err = 0;
+
+    if (len == 0)
+        len = 4096;
+
+    s.resize(len);
+
+    do {
+        bytes = (int)::recv(sock, s.data(), len, 0);
+
+        if (bytes == -1)
+            err = errno;
+    } while (bytes == -1 && err == EWOULDBLOCK);
+
+    if (bytes == 0 || (bytes == -1 && err == ECONNRESET)) {
+        open = false;
+        return "";
+    } else if (bytes == -1)
+        throw formatted_error(FMT_STRING("recv failed ({})"), err);
+
+    return s.substr(0, bytes);
+}
+
+void client_thread::handle_packet(const string_view& packet) {
+    fmt::print("FIXME - handle packet\n");
+}
+
 void client_thread::run() {
-    fprintf(stderr, "???\n");
+    while (open) {
+        while (buf.length() < sizeof(tds_header)) {
+            buf += recv((unsigned int)(sizeof(tds_header) - buf.length()));
+
+            if (!open)
+                break;
+        }
+
+        auto& h = *(tds_header*)buf.data();
+        uint16_t len = __builtin_bswap16(h.length);
+
+        if (len < sizeof(tds_header))
+            throw formatted_error(FMT_STRING("Packet length was {}, expected at least {}."), len, sizeof(tds_header));
+
+        while (buf.length() < len) {
+            buf += recv((unsigned int)(len - buf.length()));
+
+            if (!open)
+                break;
+        }
+
+        // FIXME - large packets
+
+        handle_packet(string_view(buf).substr(0, len));
+
+        buf = buf.substr(len);
+    }
+
+    // FIXME - remove from client_threads list
 }
--- src/mariatds.h
+++ src/mariatds.h
@@ -1,6 +1,7 @@
 #include <string>
 #include <stdexcept>
 #include <thread>
+#include <fmt/format.h>
 
 class sockets_error : public std::exception {
 public:
@@ -16,6 +17,21 @@ private:
     std::string msg;
 };
 
+class formatted_error : public std::exception {
+public:
+	template<typename T, typename... Args>
+	formatted_error(const T& s, Args&&... args) {
+		msg = fmt::format(s, std::forward<Args>(args)...);
+	}
+
+	const char* what() const noexcept {
+		return msg.c_str();
+	}
+
+private:
+	std::string msg;
+};
+
 class client_thread {
 public:
     client_thread(unsigned int sock) : sock(sock), t([&]() {
@@ -24,7 +40,36 @@ public:
 
 private:
     void run();
+    std::string recv(unsigned int len);
+    void handle_packet(const std::string_view& packet);
 
     unsigned int sock;
     std::thread t;
+    std::string buf;
+    bool open = true;
 };
+
+enum class tds_msg : uint8_t {
+    sql_batch = 1,
+    pretds7_login,
+    rpc,
+    tabular_result,
+    attention_signal = 6,
+    bulk_load_data,
+    federated_auth_token,
+    trans_man_req = 14,
+    tds7_login = 16,
+    sspi,
+    prelogin
+};
+
+struct tds_header {
+    enum tds_msg type;
+    uint8_t status;
+    uint16_t length;
+    uint16_t spid;
+    uint8_t packet_id;
+    uint8_t window;
+};
+
+static_assert(sizeof(tds_header) == 8, "tds_header has wrong size");
